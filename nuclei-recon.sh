3527ea73a0c8fa6603147d0cdbfbcbe63b75e1e2
André Ricardo <dedaricardo@gmail.com>
2021-08-01 22:29:42 -0300

    b

--- nuclei-recon.sh
+++ nuclei-recon.sh
@@ -31,7 +31,7 @@ rm tudo subsamass j200 tot;
 #############################################validando subdominios######################################################
 
 echo "Validando subdominios " | notify;
-cat total | httpx -threads 200 -random-agent -o 300;
+cat total | httpx -threads 300 -random-agent -o 200;
 cat 200 | anew total200; rm 200;
 #cat total200 | sed 's/$/\//' | anew total200Barra;
 #cat total | httprobe -p http:81 -p http:3000 -p https:3000 -p http:3001 -p https:3001 -p http:8000 -p http:8080 -p https:8443 -p https:10000 -p http:9000 -p https:9443 -c 300 | anew total200;
