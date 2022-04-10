bf3f385a7414ce20070ffdc7e96fceb706fefb0e
André Ricardo <dedaricardo@gmail.com>
2022-04-10 15:04:10 -0300

    ajustes nos arquilos de instalações

--- installation.sh
+++ installation.sh
@@ -15,13 +15,13 @@ apt-get install host -y;
 go mod init puppy;
 apt-get install wget -y;
 apt-get install tmux -y; 
-go get -u github.com/tomnomnom/anew
-go get -u github.com/hiddengearz/jsubfinder
-GO111MODULE=off go get -v github.com/projectdiscovery/shuffledns/cmd/shuffledns;
+go install -v github.com/tomnomnom/anew@latest;
+go install github.com/hiddengearz/jsubfinder@latest;
+wget https://raw.githubusercontent.com/ThreatUnkown/jsubfinder/master/.jsf_signatures.yaml && mv .jsf_signatures.yaml ~/.jsf_signatures.yaml;
+go install -v github.com/projectdiscovery/shuffledns/cmd/shuffledns@latest;
 pip3 install requests;
-go get -u github.com/tomnomnom/assetfinder;
-GO111MODULE=off go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder;
-go get github.com/003random/getJS;
+go install -v github.com/tomnomnom/assetfinder@latest;
+go install github.com/003random/getJS@latest;
 wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux;
 chmod +x findomain-linux;
 mv findomain-linux findomain; mv findomain /usr/bin;
@@ -36,27 +36,27 @@ cd ~/tools; git clone https://github.com/m4ll0k/takeover.git;
 cd ~/tools; git clone https://github.com/devanshbatham/ParamSpider.git; pip3 install -r ParamSpider/requirements.txt;
 cd ~/tools; git clone https://github.com/GerbenJavado/LinkFinder.git; pip3 install -r LinkFinder/requirements.txt;
 cd ~/tools; git clone https://github.com/m4ll0k/SecretFinder.git; pip3 install -r SecretFinder/requirements.txt;
-cd ~/tools; git clone https://github.com/KingOfBugbounty/Bug-Bounty-Toolz.git
-cd ~/tools; git clone https://github.com/dedezinholindo/BugBountyTools.git;
+cd ~/tools; git clone https://github.com/KingOfBugbounty/Bug-Bounty-Toolz.git;
 cd ~; git clone https://github.com/projectdiscovery/nuclei-templates.git;
 cd ~/tools; git clone https://github.com/0x240x23elu/JSScanner.git; pip3 install -r JSScanner/requirements.txt
 cd ~/tools; git clone https://github.com/s0md3v/Arjun.git; cd Arjun; python3 setup.py install;
 cd ~/tools; git clone https://github.com/blechschmidt/massdns.git; cd massdns; make; mv bin/massdns /usr/bin/;
-GO111MODULE=off go get -v github.com/projectdiscovery/chaos-client/cmd/chaos;
-GO111MODULE=off go get -v github.com/projectdiscovery/httpx/cmd/httpx;
+go install -v github.com/projectdiscovery/chaos-client/cmd/chaos@latest;
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest;
 GO111MODULE=off go get -u -v github.com/lc/subjs;
-go get -v github.com/OWASP/Amass/v3/...; #configuracoes
-go get -u github.com/tomnomnom/httprobe;
-go get github.com/tomnomnom/waybackurls;
-GO111MODULE=off go get -u -v github.com/bp0lr/gauplus;
-GO111MODULE=off go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei;
-cd ~/tools; git clone https://github.com/hahwul/dalfox; cd dalfox; git pull -v; go install; go build;  ; #editar payloads
-go get -u github.com/KathanP19/Gxss;
-go get -u github.com/tomnomnom/gf; 
-go get -u github.com/ffuf/ffuf;
+snap install amass; #configuracoes
+go install github.com/tomnomnom/httprobe@latest;
+go install github.com/tomnomnom/waybackurls@latest;
+go install github.com/bp0lr/gauplus@latest;
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest;
+go install github.com/hahwul/dalfox/v2@latest;
+#cd ~/tools; git clone https://github.com/hahwul/dalfox; cd dalfox; git pull -v; go install; go build; #editar payloads
+go install github.com/KathanP19/Gxss@latest;
+go install -v github.com/tomnomnom/gf@latest; 
+go install github.com/ffuf/ffuf@latest;
 cd ~/tools; git clone https://github.com/1ndianl33t/Gf-Patterns;
 cd ~; mkdir .gf;
 mv ~/tools/Gf-Patterns/*.json ~/.gf;
-GO111MODULE=off go get -v github.com/projectdiscovery/notify/cmd/notify; #configurar
+go install -v github.com/projectdiscovery/notify/cmd/notify@latest; #configurar
 
 mv ~/go/bin/* /usr/bin/;
